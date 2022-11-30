57a31de5687e324db6f4973874348fbf2a579631
Shiyun <lushiyun@outlook.com>
2022-11-30 02:35:45 -0500

    Add dom elements

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,34 @@
 <script>
 	import { Canvas } from '@threlte/core';
+  import Description from './Description.svelte';
 	import Scene from './Scene.svelte';
 
 	let scrollY = 0.1;
-	let innerHeight;
+	let innerHeight = 0;
 </script>
 
 <svelte:window bind:scrollY bind:innerHeight />
 
 <div style="height: {`${innerHeight * 26}px`}" />
 
+<nav>
+	<h1>女 /NV</h1>
+</nav>
+
 <div class="wrapper">
 	<Canvas>
 		<Scene timer={scrollY / innerHeight} />
 	</Canvas>
 </div>
+
+<Description timer={scrollY / innerHeight} />
+
+<footer>
+	<a href="https://www.shiyunlu.com" target="_blank" rel="noopener noreferrer">
+		Made by Shiyun.
+	</a>
+
+</footer>
 
 <style>
 	.wrapper {
@@ -23,6 +37,36 @@
 		left: 0;
 		height: 100%;
 		width: 100%;
-		z-index: 1;
+	}
+
+	nav {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		padding-top: 45px;
+		padding-inline: 45px;
+	}
+
+	p {
+		position: fixed;
+		bottom: 60px;
+
+	}
+
+	footer {
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		width: 100%;
+		padding-bottom: 45px;
+		padding-inline: 45px;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	h1 {
+		font-weight: 38px;
 	}
 </style>
---
+++ src/Description.svelte
@@ -0,0 +1,35 @@
+<script>
+	import data from './data.json';
+
+	export let timer;
+
+	let index = Math.floor(timer / 2);
+	let hanzi = data?.[index]?.hanzi;
+	let pinyin = data?.[index]?.pinyin;
+	let english = data?.[index]?.english;
+</script>
+
+<p>
+	<span>/ {hanzi}</span>
+	<span>/ {pinyin}</span>
+	<span>/ {english}</span>
+</p>
+
+<style>
+	p {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		font-size: 2rem;
+	}
+
+	span {
+		display: block;
+	}
+</style>
